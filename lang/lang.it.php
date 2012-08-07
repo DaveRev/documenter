740f2ab783814322495de798b0b9e0b986f2ea9e
Davide <dadeg90@hotmail.it>
2012-08-07 16:44:02 +0200

    Update italian translation (incomplete)

---
+++ lang/lang.it.php
@@ -0,0 +1,69 @@
+<?php
+
+	$about = array(
+		'name' => 'Italian',
+		'author' => array(
+			'name' => 'Davide Grobberio',
+			'email' => 'davide.grobberio@gmail.com',
+			'website' => ''
+		),
+		'release-date' => '2012-07-30'
+	);
+
+	/**
+	 * Documenter
+	 */
+	$dictionary = array(
+
+		'Button Text' => 
+		'Button Text',
+
+		'Content is a required field' => 
+		'Nessun contentut presente.',
+
+		'Create a new documentation item' => 
+		'Crea nuovo',
+
+		'Delete this template' => 
+		'Diese Vorlage löschen',
+
+		'Document It' => 
+		'Salva',
+
+		'Documentation' => 
+		'Documentazione',
+
+		'Documentation Item not found' => 
+		'documentazione non trovata',
+
+		'Page is a required field' => 
+		'la pagina è un campo richiesto',
+
+		'Pages is a required field' => 
+		'le pagine sono un campo richiesto',
+
+		'The documentation item you requested to edit does not exist.' => 
+		'La documentazione richiesta non esiste. ',
+
+		'Show' => 
+		'mostra',
+
+		'Hide' => 
+		'nascondi',
+
+		'Unknown errors occurred while attempting to save. Please check your <a href="%s">activity log</a>.' => 
+		false,
+
+		'An error occurred while processing this form. <a href="#error">See below for details.</a>' => 
+		false,
+
+		'Title is a required field' => 
+		false,
+
+		'Documentation Item updated at %1$s. <a href="%2$s">Create another?</a> <a href="%3$s">View all Documentation</a>' =>
+		'Documentazione aggiornata %1$s. <a href="%2$s">Crearne una nuova?</a> <a href="%3$s">Vedi tutte.</a>',
+
+		'Documentation Item created at %1$s. <a href="%2$s">Create another?</a> <a href="%3$s">View all Documentation</a>' => 
+		'Documentazione creata %1$s. <a href="%2$s">Crearne una nuova?</a> <a href="%3$s">Vedi tutte.</a>',
+
+	);
